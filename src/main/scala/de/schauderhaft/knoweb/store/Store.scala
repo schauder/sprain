bded89743e41b94bb2684f367f8cb5bc77a841a7
Jens Schauder <jens.schauder@freenet.de>
2012-06-17 11:47:40 +0200

    extracted session handling from Store; added Links to store

--- src/main/scala/de/schauderhaft/knoweb/store/Store.scala
+++ src/main/scala/de/schauderhaft/knoweb/store/Store.scala
@@ -3,28 +3,50 @@ package de.schauderhaft.knoweb.store
 import org.scalaquery.ql.basic.{ BasicTable => Table }
 import org.scalaquery.ql.TypeMapper._
 import org.scalaquery.ql._
-import org.scalaquery.session.Database.threadLocalSession
 import org.scalaquery.ql.basic.BasicDriver.Implicit._
 import org.scalaquery.session._
+import java.util.UUID
 
-class Store(val db : Database) {
+class Store(sessionFunc : => Session) {
+
+    implicit def session = sessionFunc
 
     def allNodes() = {
-        db withSession {
-            val nodes = for (n <- Nodes) yield n.name
-            nodes.list.toSet
-        }
+        val nodes = for (n <- Nodes) yield n.name
+        nodes.list.toSet
     }
 
-    def allLinks() = Set()
+    def allLinks() = (
+        for (l <- Links)
+            yield (l.from, l.link, l.to))
+        .list.toSet
+
+    /** adds a node to the store */
     def add(node : String) {
-        db withSession {
-            Nodes.insert(node)
-        }
+        Nodes.insert(UUID.randomUUID.toString, node)
+    }
+
+    /** adds a link between to nodes to the store */
+    def add(from : String, link : String, to : String) {
+        Links.insert(UUID.randomUUID.toString, from, link, to)
     }
 }
 
-object Nodes extends Table[(String)]("node") {
-    def name = column[String]("name")
-    def * = name
+object Nodes extends Table[(String, String)]("node") {
+    def id = column[String]("id", O.PrimaryKey)
+    def name = column[String]("name", O.NotNull)
+
+    def * = id ~ name
+    private def uName = index("uk_name", name, unique = true)
+}
+
+object Links extends Table[(String, String, String, String)]("link") {
+    def id = column[String]("id", O.PrimaryKey)
+    def from = column[String]("from", O.NotNull)
+    def link = column[String]("name", O.NotNull)
+    def to = column[String]("to", O.NotNull)
+
+    def * = id ~ from ~ link ~ to
+    private def uFromLinkTo = index("uk_FromLinkTo", from ~ link ~ to, unique = true)
+
 }
--- src/test/scala/de/schauderhaft/knoweb/store/StoreTest.scala
+++ src/test/scala/de/schauderhaft/knoweb/store/StoreTest.scala
@@ -6,40 +6,73 @@ import org.scalatest.FunSuite
 import org.scalaquery.session._
 import org.scalaquery.session.Database.threadLocalSession
 import org.scalaquery.ql.basic.BasicDriver.Implicit._
+import org.scalatest.BeforeAndAfter
+import org.scalaquery.session.Database.threadLocalSession
 
 @RunWith(classOf[JUnitRunner])
-class StoreTest extends FunSuite {
+class StoreTest extends FunSuite with BeforeAndAfter {
     import org.scalatest.matchers.ShouldMatchers._
 
     val db = Database.forURL("jdbc:h2:mem:test1;DB_CLOSE_DELAY=-1", driver = "org.h2.Driver")
+
     db withSession {
-        Nodes.ddl create
+        (Nodes.ddl ++ Links.ddl).create
+        // println(Nodes.ddl.createStatements.mkString("/n"))
     }
 
-    test("new store does not contain nodes") {
-        val store = new Store(db)
+    def txtest(name : String)(t : => Any) {
+        test(name) {
+            db withTransaction {
+                try t
+                finally threadLocalSession.rollback
+            }
+        }
+    }
+
+    txtest("new store does not contain nodes") {
+        val store = new Store(threadLocalSession)
         store.allNodes should equal(Set())
     }
 
-    test("new store does not contain links") {
-        val store = new Store(db)
-        store.allLinks should equal(Set())
-    }
-
-    test("a store contains nodes added to it") {
-        val store = new Store(db)
+    txtest("a store contains nodes added to it") {
+        val store = new Store(threadLocalSession)
         store.add("a")
         store.add("b")
 
         store.allNodes should equal(Set("a", "b"))
     }
 
-    test("nodes in a store can be retrieved using a different store") {
-        val store = new Store(db)
+    txtest("nodes in a store can be retrieved using a different store") {
+        val store = new Store(threadLocalSession)
         store.add("a")
         store.add("b")
 
-        new Store(db).allNodes should equal(Set("a", "b"))
+        new Store(threadLocalSession).allNodes should equal(Set("a", "b"))
+    }
+
+    txtest("adding a node is a projection") {
+        val store = new Store(threadLocalSession)
+        store.add("a")
+        store.add("a")
+
+        store.allNodes should equal(Set("a"))
+    }
+
+    txtest("new store does not contain links") {
+        val store = new Store(threadLocalSession)
+        store.allLinks should equal(Set())
+    }
+
+    txtest("a store contains the links added to it") {
+        val store = new Store(threadLocalSession)
+
+        store.add("a")
+        store.add("b")
+        store.add("c")
+
+        store.add("a", "to", "b")
+
+        store.allLinks should equal(Set(("a", "to", "b")))
     }
 
 }
