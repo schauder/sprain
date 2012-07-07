5b409e0d8ea0d2751075cfc2739ae179b77c2239
Jens Schauder <jens@schauderhaft.de>
2012-07-07 11:35:12 +0200

    simpleton test for existing of the add node form

--- src/test/scala/de/schauderhaft/sprain/view/GraphJadeTest.scala
+++ src/test/scala/de/schauderhaft/sprain/view/GraphJadeTest.scala
@@ -14,8 +14,17 @@ class GraphJadeTest extends FunSuite {
     val uri = "src/main/webapp/WEB-INF/views/graph.jade"
     val engine = new TemplateEngine
     val template = engine.load(uri)
+
     test("rendering without parameter") {
         val output = engine.layout(uri, template, Map[String, Any]())
+    }
+
+    test ("rendering add nodes form"){
+        val output = engine.layout(uri, template, Map[String, Any]())
+
+	output should include regex pattern("form nodes/new")
+	output should include regex pattern("input nodeName")
+	output should include regex pattern("input submit")
     }
 
     test("rendering nodes") {
@@ -28,7 +37,14 @@ class GraphJadeTest extends FunSuite {
         output should include("2")
         output should include("alpha")
         output should include("beta")
+    }
 
+    test ("rendering add link form per node"){
+        pending
+    }
+
+    test ("rendering delete node form per node"){
+        pending
     }
 
     test("rendering links") {
@@ -42,4 +58,11 @@ class GraphJadeTest extends FunSuite {
         output should include("likes")
         output should include("loves")
     }
+
+    
+    test ("rendering delete link form per link"){
+        pending
+    }
+
+    def pattern(pat : String) = pat.replace(" ", ".*").r
 }
