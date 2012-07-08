f4bc2bc28c8c2653e5a509b34e4b92a6cab3be1a
Jens Schauder <jens@schauderhaft.de>
2012-07-08 18:38:22 +0200

    implemented first primitive test for request mapping

---
+++ src/test/scala/de/schauderhaft/sprain/GraphControllerMappingTest.scala
@@ -0,0 +1,27 @@
+package de.schauderhaft.sprain
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.mock.MockitoSugar
+import org.springframework.mock.web.MockHttpServletRequest
+import org.springframework.mock.web.MockHttpServletResponse
+import org.springframework.test.web.server.setup.MockMvcBuilders
+import org.springframework.test.web.server.request.MockMvcRequestBuilders._
+import org.springframework.test.web.server.result.MockMvcResultMatchers._
+@RunWith(classOf[JUnitRunner])
+class GraphControllerMappingTest extends FunSuite with MockitoSugar {
+    import org.scalatest.matchers.ShouldMatchers._
+
+    test("/ is mapped to home method") {
+	
+        pending
+
+        val controller = mock[GraphController]
+
+        MockMvcBuilders.standaloneSetup(controller).build().
+            perform(get("/")).
+            andExpect(status().isOk()).
+            andExpect(content().`type`("text/plain")).
+            andExpect(content().string("hello world"))    
+    }
+}
