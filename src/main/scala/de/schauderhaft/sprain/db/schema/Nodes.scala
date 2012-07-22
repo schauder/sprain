291dcbf62e7f35e828956525189440281a002004
Jens Schauder <jens.schauder@freenet.de>
2012-07-22 21:26:11 +0200

    added coomment with sql script

--- src/main/scala/de/schauderhaft/sprain/db/schema/Nodes.scala
+++ src/main/scala/de/schauderhaft/sprain/db/schema/Nodes.scala
@@ -1,11 +1,29 @@
 package de.schauderhaft.sprain.db.schema
 import org.scalaquery.ql.basic.{ BasicTable => Table }
 
-object Nodes extends Table[(String, String)]("node") {
-    def id = column[String]("id", O.PrimaryKey)
-    def name = column[String]("name", O.NotNull)
+/*
+create table node(
+ id varchar(255) PRIMARY KEY ,
+ name varchar(255) not null
+);
 
-    def * = id ~ name
-    private def uName = index("uk_name", name, unique = true)
+
+create table link(
+ id varchar(255) PRIMARY KEY ,
+ from_id varchar(255) not null,
+ link varchar(255) not null,
+ to_id varchar(255) not null,
+ -- CONSTRAINT ukFromLinkTo UNIQUE (from_id, link, to_id),
+ CONSTRAINT fkFrom FOREIGN KEY (from_id) REFERENCES node (id),
+ CONSTRAINT fkTo FOREIGN KEY (from_id) REFERENCES node (id)
+);
+*/
+
+object Nodes extends Table[(String, String)]("node") {
+  def id = column[String]("id", O.PrimaryKey)
+  def name = column[String]("name", O.NotNull)
+
+  def * = id ~ name
+  private def uName = index("uk_name", name, unique = true)
 }
 
