942d45f1cba5391f74f9c56678e04437c11ef548
Jens Schauder <jschauder@vmware.com>
2021-06-22 16:58:12 +0200

    Project setup.

---
+++ src/test/java/de/schauderhaft/sprain/SprainApplicationTests.java
@@ -0,0 +1,13 @@
+package de.schauderhaft.sprain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
